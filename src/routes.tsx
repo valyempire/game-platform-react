6fd6286f8d63baca86f851e66630904b067a4f74
unknown <valy_empire@yahoo.com>
2024-02-10 11:54:58 +0200

    Fetch game details

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "game/:id", element: <GameDetailsPaige /> },
+      { path: "games/:slug", element: <GameDetailsPaige /> },
     ],
   },
 ]);
